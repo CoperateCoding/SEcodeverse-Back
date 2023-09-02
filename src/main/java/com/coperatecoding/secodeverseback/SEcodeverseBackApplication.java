0ecc18ae1374b22cb0c2cc3475876c8f5c9314d8
eunslog <yeseo235@gmail.com>
2023-09-02 19:29:43 +0900

    first

---
+++ src/main/java/com/coperatecoding/secodeverseback/SEcodeverseBackApplication.java
@@ -0,0 +1,13 @@
+package com.coperatecoding.secodeverseback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEcodeverseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SEcodeverseBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coperatecoding/secodeverseback/SEcodeverseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coperatecoding.secodeverseback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SEcodeverseBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
