ff35b4bb5aeb65dcc7a57ecdc2968cc6bc059b3f
chohuison <117262031+chohuison@users.noreply.github.com>
2023-10-19 19:15:58 +0900

    feat/categoryQuestion

--- src/main/java/com/coperatecoding/secodeverseback/controller/QuestionController.java
+++ src/main/java/com/coperatecoding/secodeverseback/controller/QuestionController.java
@@ -7,10 +7,7 @@ import com.coperatecoding.secodeverseback.domain.question.Question;
 import com.coperatecoding.secodeverseback.dto.*;
 import com.coperatecoding.secodeverseback.exception.CategoryNotFoundException;
 import com.coperatecoding.secodeverseback.exception.ForbiddenException;
-import com.coperatecoding.secodeverseback.service.BoardService;
-import com.coperatecoding.secodeverseback.service.LevelService;
-import com.coperatecoding.secodeverseback.service.QuestionService;
-import com.coperatecoding.secodeverseback.service.TestCaseService;
+import com.coperatecoding.secodeverseback.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -29,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,6 +43,8 @@ public class QuestionController {
     private final QuestionService questionService;
     private final TestCaseService testCaseService;
     private final LevelService levelService;
+    private final QuestionCategoryService questionCategoryService;
+
     @PostMapping("/post")
     public ResponseEntity makeQuestion(@AuthenticationPrincipal User user, @RequestBody QuestionandTestCaseDTO.AddQuestionAndTestRequest addQuestionRequest) {
         Question question = questionService.makeQuestion(user, addQuestionRequest.getQuestion());
@@ -55,35 +55,66 @@ public class QuestionController {
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
     @GetMapping("")
-    public ResponseEntity getQuestions(){
-        List<QuestionDTO.SearchQuestionListResponse>  questions = questionService.getQuestion();
-        List<Map<String, Object>> questionsAndLevels = new ArrayList<>();
-        for(QuestionDTO.SearchQuestionListResponse  question: questions){
-            Level level = levelService.getLevel(question.getLevelPk());
+    public ResponseEntity<List<QuestionDTO.SearchQuestionListResponse>> getQuestions(
+            @RequestParam(value = "q", required = false) String q,
+            @RequestParam(value = "sort", required = false) SortType sort,
+            @RequestParam(value = "categoryPk", required = false) List<Long> categoryPks,
+            @RequestParam(value = "levelPk", required = false) List<Long> levelPks
+    ) {
 
-            Map<String, Object> questionAndLevelMap = new HashMap<>();
-            questionAndLevelMap.put("question", question);
-            questionAndLevelMap.put("level", level);
+        List<QuestionDTO.SearchQuestionListResponse> questions = new ArrayList<>();
 
-            questionsAndLevels.add(questionAndLevelMap);
+
+            if (sort != null) {
+            if (categoryPks != null && !categoryPks.isEmpty() && levelPks != null && !levelPks.isEmpty()) {
+                for (Long categoryPk : categoryPks) {
+                    for (Long levelPk : levelPks) {
+                        List<QuestionDTO.SearchQuestionListResponse> matchingQuestions = questionService.getMatchingQuestions(true, categoryPk, levelPk);
+                        questions.addAll(matchingQuestions);
+                    }
+                }
+            } else if (categoryPks != null && !categoryPks.isEmpty()) {
+                for (Long categoryPk : categoryPks) {
+                    List<QuestionDTO.SearchQuestionListResponse> categoryQuestions = questionService.getCategoryQuestion(true, categoryPk);
+                    questions.addAll(categoryQuestions);
+                }
+            } else if (levelPks != null && !levelPks.isEmpty()) {
+                for (Long levelPk : levelPks) {
+                    List<QuestionDTO.SearchQuestionListResponse> levelQuestions = questionService.getLevelQuestionList(true, levelPk);
+                    questions.addAll(levelQuestions);
+                }
+            }
+        } else {
+            if (categoryPks != null && !categoryPks.isEmpty() && levelPks != null && !levelPks.isEmpty()) {
+                for (Long categoryPk : categoryPks) {
+                    for (Long levelPk : levelPks) {
+                        List<QuestionDTO.SearchQuestionListResponse> matchingQuestions = questionService.getMatchingQuestions(false, categoryPk, levelPk);
+                        questions.addAll(matchingQuestions);
+                    }
+                }
+            } else if (categoryPks != null && !categoryPks.isEmpty()) {
+                for (Long categoryPk : categoryPks) {
+                    List<QuestionDTO.SearchQuestionListResponse> categoryQuestions = questionService.getCategoryQuestion(false, categoryPk);
+                    questions.addAll(categoryQuestions);
+                }
+            } else if (levelPks != null && !levelPks.isEmpty()) {
+                for (Long levelPk : levelPks) {
+                    List<QuestionDTO.SearchQuestionListResponse> levelQuestions = questionService.getLevelQuestionList(false, levelPk);
+                    questions.addAll(levelQuestions);
+                }
+            }
+            if (questions.isEmpty()) {
+                questions = questionService.getQuestion();
+
+            }
         }
-        return ResponseEntity.ok(questionsAndLevels);
-    }
-    @GetMapping("/category = {categoryPk}")
-    public ResponseEntity getCategoryQuestions(Long categoryPk){
-        List<QuestionDTO.SearchQuestionListResponse>  questions = questionService.getCategoryQuestion(categoryPk);
-        List<Map<String, Object>> questionsAndLevels = new ArrayList<>();
-        for(QuestionDTO.SearchQuestionListResponse  question: questions){
-            Level level = levelService.getLevel(question.getLevelPk());
 
-            Map<String, Object> questionAndLevelMap = new HashMap<>();
-            questionAndLevelMap.put("question", question);
-            questionAndLevelMap.put("level", level);
-
-            questionsAndLevels.add(questionAndLevelMap);
+            return ResponseEntity.ok(questions);
         }
-        return ResponseEntity.ok(questionsAndLevels);
+
     }
 
-}
+
+
--- src/main/java/com/coperatecoding/secodeverseback/repository/QuestionRepository.java
+++ src/main/java/com/coperatecoding/secodeverseback/repository/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.coperatecoding.secodeverseback.repository;
 
 import com.coperatecoding.secodeverseback.domain.Comment;
 import com.coperatecoding.secodeverseback.domain.board.Board;
+import com.coperatecoding.secodeverseback.domain.question.Level;
 import com.coperatecoding.secodeverseback.domain.question.Question;
 import com.coperatecoding.secodeverseback.domain.question.QuestionCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-        List<Question> findByCategory(QuestionCategory questionCategory);}
+        List<Question> findByCategory(QuestionCategory questionCategory);
+        List<Question> findByLevel(Level level);
+        List<Question>findByCategoryAndLevel(QuestionCategory questionCategory, Level level);
+}
+
 
