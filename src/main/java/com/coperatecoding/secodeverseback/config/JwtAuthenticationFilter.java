c6704e8bd607511ce7379ec39cfe108415461264
eunslong <yeseo235@gmail.com>
2023-11-01 21:42:12 +0900

    feat/yml/jwt

--- src/main/java/com/coperatecoding/secodeverseback/config/JwtAuthenticationFilter.java
+++ src/main/java/com/coperatecoding/secodeverseback/config/JwtAuthenticationFilter.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Slf4j
