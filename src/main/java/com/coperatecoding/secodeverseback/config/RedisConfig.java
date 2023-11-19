89a61eb5d175cd096c8003e2ef1cfdac5c3bbf03
chohuison <117262031+chohuison@users.noreply.github.com>
2023-11-20 01:43:45 +0900

    redis

---
+++ src/main/java/com/coperatecoding/secodeverseback/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.coperatecoding.secodeverseback.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    public String redisHost;
+    @Value("${spring.redis.port}")
+    public String redisPort;
+
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
