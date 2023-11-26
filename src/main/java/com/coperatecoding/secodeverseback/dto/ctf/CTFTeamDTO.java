2faaf05536879df37a93f563cfd0430084964c89
eunslog <yeseo235@gmail.com>
2023-11-27 03:53:10 +0900

    feat/ctfTeam

--- src/main/java/com/coperatecoding/secodeverseback/dto/ctf/CTFTeamDTO.java
+++ src/main/java/com/coperatecoding/secodeverseback/dto/ctf/CTFTeamDTO.java
@@ -1,9 +1,12 @@
 package com.coperatecoding.secodeverseback.dto.ctf;
 
+import com.coperatecoding.secodeverseback.dto.board.BoardDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 
 public class CTFTeamDTO {
@@ -18,10 +21,45 @@ public class CTFTeamDTO {
 
     @Getter
     @NoArgsConstructor
+    public static class JoinRequest {
+        private Long teamPk;
+        private String pw;
+    }
+
+    @Getter
+    @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class DetailResponse {
         private String name;
+        private Integer score;
+        private Integer teamRank;
     }
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class SearchListResponse {
+        private int cnt;
+        private List<CTFTeamDTO.SearchResponse> list;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Top10ListResponse {
+        private int cnt;
+        private List<CTFTeamDTO.DetailResponse> list;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class SearchResponse {
+        private String name;
+        private List<String> memberList;
+    }
 }
--- src/main/java/com/coperatecoding/secodeverseback/repository/CTFTeamRepository.java
+++ src/main/java/com/coperatecoding/secodeverseback/repository/CTFTeamRepository.java
@@ -1,7 +1,15 @@
 package com.coperatecoding.secodeverseback.repository;
 
 import com.coperatecoding.secodeverseback.domain.ctf.CTFTeam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface CTFTeamRepository extends JpaRepository<CTFTeam, Long> {
+    Page<CTFTeam> findByLeaguePk(Long leaguePk, Pageable pageable);
+
+    List<CTFTeam> findTop10ByLeaguePkOrderByTeamRankAsc(Long leaguePk);
 }
